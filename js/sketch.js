e4df3aaeb0e1eda19a064626243ca3b66614b9fb
jmnuf <59884263+jmnuf@users.noreply.github.com>
2020-10-04 18:56:43 +0200

    Updated sketch.js
    Re-did how sliders are added and changed the material used on the models

--- js/sketch.js
+++ js/sketch.js
@@ -27,58 +27,87 @@ function setup() {
   // Human model:
   // https://clara.io/view/23d8ae52-6bb1-4c5a-8f69-365640f48893#
   loadModel('assets/objs/standard-male-figure.obj', (model) => {
-  	person.model = model;
-  	console.log('Model for average human has been loaded');
+	person.model = model;
+	console.log('Model for average human has been loaded');
   });
   // a380 model:
   // https://skfb.ly/6SBUC
   loadModel('assets/objs/a-three-eighty.obj', (model) => {
-  	chunkyBoi.model = model;
-  	console.log('Model for a380 has been loaded');
+	chunkyBoi.model = model;
+	console.log('Model for a380 has been loaded');
   });
-  let sliderTags = [ 'x: ', 'y: ', 'z: ' ];
-  for(let i = 0; i < sliderTags.length; i++) {
-  	let div = createDiv(sliderTags[i]);
-  	div.class('container-fluid');
-  	const max = 1000;
-  	let slider = createSlider(-max, max, 0, 0.5)
-  	slider.style('width', `90%`);
-  	slider.parent(div);
-  	sliders.push(slider);
-  	div.parent(main);
+  const max = 1000, step = 0.5;
+  // let sliderTags = [ 'x: ', 'y: ', 'z: ' ];
+  let sliderTags = {
+	Translation: {
+	  x: {
+		max: 600, initVal: 0, step
+	  },
+	  y: {
+		max: 600, initVal: 10, step
+	  },
+	  z: {
+		max:890, initVal: 890, step
+	  }
+	}
   }
-  sliders[1].value(10);
-  sliders[2].value(890);
+  for(let Type in sliderTags) {
+	let typeDiv = createDiv(`<h4>${Type}</h4>`);
+	typeDiv.class('container');
+	for(sliderN in sliderTags[Type]) {
+		const sliderCfg = sliderTags[Type][sliderN];
+		const type = Type.toLowerCase();
+		let sliderDiv = createDiv();
+		// sliderDiv.class('container-fluid');
+		let sliderLbl = createElement('label', `<h6>${sliderN}: ${sliderCfg.initVal}</h6>`);
+		sliderLbl.attribute('for', `${type}-${sliderN}`)
+		let slider = createSlider(-sliderCfg.max, sliderCfg.max, sliderCfg.initVal, sliderCfg.step);
+		slider.id(`${type}-${sliderN}`);
+		slider.class('custom-range');
+		slider.style('width', `${width}px`);
+		sliderLbl.parent(sliderDiv);
+		slider.parent(sliderLbl);
+		sliderDiv.parent(typeDiv);
+		sliders.push({slide:slider, label:select(`label[for="${type}-${sliderN}"] h6`)});
+	}
+	typeDiv.parent(main);
+  }
   angleMode(DEGREES);
 }
 
 function draw() {
   background(51);
+  noStroke();
   for(let i = 0; i < translation.length; i++) {
-  	translation[i] = sliders[i].value();
+	translation[i] = sliders[i].slide.value();
+	let prev = sliders[i].label.html();
+	sliders[i].label.html(`${prev.substr(0, prev.indexOf(':') + 1)} ${translation[i]}`);
   }
+  ambientLight(60);
+  pointLight(255, 255, 255, -translation[0], translation[1], translation[2] + 100);
   translate(-translation[0], translation[1], translation[2]);
   // rotateY(90);
 
 
   if (person.model) {
-  	push();
-  	rotateZ(180);
-  	translate(person.pos.x, person.pos.x, person.pos.z);
-  	normalMaterial();
-  	model(person.model, true);
-  	pop();
+	push();
+	rotateZ(180);
+	translate(person.pos.x, person.pos.x, person.pos.z);
+	ambientMaterial(200);
+	// specularMaterial(250);
+	model(person.model, true);
+	pop();
   }
 
   if (chunkyBoi.model) {
-  	push();
-  	translate(chunkyBoi.pos.x, chunkyBoi.pos.y, chunkyBoi.pos.z);
-  	rotateY(-90);
-  	rotateZ(180);
-  	scale(0.11505);
-  	// scale(0.00885); // Size of human model
-  	normalMaterial();
-  	model(chunkyBoi.model, true);
-  	pop();
+	push();
+	translate(chunkyBoi.pos.x, chunkyBoi.pos.y, chunkyBoi.pos.z);
+	rotateY(-90);
+	rotateZ(180);
+	scale(0.11505);
+	// scale(0.00885); // Size of human model
+	ambientMaterial(250);
+	model(chunkyBoi.model, true);
+	pop();
   }
 }
