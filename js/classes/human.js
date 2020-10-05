279589050540b8825310f5a2b38db0af6138fffa
jmnuf <59884263+jmnuf@users.noreply.github.com>
2020-10-05 23:15:20 +0200

    Create human.js
    Created a class for the human 3D model

---
+++ js/classes/human.js
@@ -0,0 +1,31 @@
+class Human extends JM_3DBasics {
+	constructor(model) {
+		super(0, 0, -540);
+		this.model = model;
+		this.rotate.z = 180;
+		this.texture = null;
+	}
+
+	move(x, y, z) {
+		this.pos.add(x, y, z);
+	}
+
+	render() {
+		push();
+		rotationOfSelf();
+		translateToSelf();
+		if (JM_3DBasics.isValidTexture(this.texture)) {
+			texture(this.texture);
+		}
+		ambientMaterial(200);
+		model(this.model);
+		pop();
+	}
+
+	copy() {
+		cpy = new Human(this.model);
+		cpy.pos = this.pos.copy();
+		cpy.rotate = this.rotate.copy();
+		return cpy;
+	}
+}
